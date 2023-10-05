d219a17c943cb2c4590a07729c42b5c0a18c981a
antiyro <antiyro@gmail.com>
2023-10-05 09:35:45 +0200

    basic config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 import './styles.css';
+import './barknet.png';
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,32 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   entry: './src/index.ts',
   mode: 'development',
   module: {
     rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+        {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+                {
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'images/'
+                }
+                },
+            ],
+        },                    
+        {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        },
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
     ],
   },
   resolve: {
@@ -27,5 +40,9 @@ module.exports = {
   devServer: {
     static: './dist',
     historyApiFallback: true,
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },  
 };
